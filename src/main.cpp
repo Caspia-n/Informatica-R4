86d93a580309ff88420ae3d1b11d4779516894f1
fancypants-goat <edingmichiel1@gmail.com>
2025-10-16 14:44:00 +0200

    Implement code changes to enhance functionality and improve performance

--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,11 @@ Servo servo1, servo2, servo3, servo4, servo5;
 
 void setup() {
     Serial.begin(115200);
-    servo1.attach(11); // Index
-    servo2.attach(10); // Middle
-    servo3.attach(9);  // Ring
-    servo4.attach(6);  // Pinky
-    servo5.attach(5);  // Thumb
+    servo1.attach(10); // Index
+    servo2.attach(9); // Middle
+    servo3.attach(6);  // Ring
+    servo4.attach(5);  // Pinky
+    servo5.attach(3);  // Thumb
 }
 
 void loop() {
@@ -23,16 +23,17 @@ void loop() {
             servo3.write(a3);
             servo4.write(a4);
             servo5.write(a5);
-            Serial.print("Servos set to: ");
-            Serial.print(a1);
-            Serial.print(", ");
-            Serial.print(a2);
-            Serial.print(", ");
-            Serial.print(a3);
-            Serial.print(", ");
-            Serial.print(a4);
-            Serial.print(", ");
-            Serial.println(a5);
+            
+            // print servo angles
+            char headersBuffer[64];
+            char anglesBuffer[64];
+            
+            sprintf(headersBuffer, "Index  | Middle | Ring   | Pinky  | Thumb");
+            sprintf(anglesBuffer, "%-6d | %-6d | %-6d | %-6d | %d", a1, a2, a3, a4, a5);
+            
+            Serial.println(headersBuffer);
+            Serial.println("------|--------|------|-------|------");
+            Serial.println(anglesBuffer);
         } else {
             Serial.print("Invalid data: ");
             Serial.println(data);
